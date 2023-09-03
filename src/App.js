57b2f62fc9b1aec3181f1b41634ac4251d24e05f
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-09-03 17:41:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
       <div className="text-[#0398d4] font-medium text-2xl">Increment && Decrement</div>
-      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[26px] text-[#344151]">
 
         <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
           -
